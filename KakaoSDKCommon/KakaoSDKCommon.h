944b49f30268c4441d334c8d4c18465b95f5ed3c
wannagohome <iwwuf7@gmail.com>
2020-09-24 15:28:57 +0900

    Initial Commit

---
+++ KakaoSDKCommon/KakaoSDKCommon.h
@@ -0,0 +1,19 @@
+//
+//  KakaoSDKCommon.h
+//  KakaoSDKCommon
+//
+//  Created by jinho jang on 2020/09/24.
+//  Copyright © 2020 People. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KakaoSDKCommon.
+FOUNDATION_EXPORT double KakaoSDKCommonVersionNumber;
+
+//! Project version string for KakaoSDKCommon.
+FOUNDATION_EXPORT const unsigned char KakaoSDKCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KakaoSDKCommon/PublicHeader.h>
+
+
